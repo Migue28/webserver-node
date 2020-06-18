aea9a6caac4b0dc70e2d0325a9ba8cf66e00277d
Eddy <miguepoke28@gmail.com>
2020-06-18 17:13:51 -0500

    First Express added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  const salida = {
+    nombre: "Eddy",
+    edad: 23,
+    url: req.url,
+  };
+  res.send(salida)
+});
+
+app.get("/user", function (req, res) {
+    res.send('Hola User')
+  });
+
+app.listen(3000, ()=> {console.log(`Escuchando puerto 3000`);});
