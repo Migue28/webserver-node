7f12d4ba2e819de029030c06c5bb0e55f775bd91
Eddy <miguepoke28@gmail.com>
2020-06-19 17:23:44 -0500

    Basic bootstrap added

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
+app.use(express.static(__dirname + '/public'))
+
+/* app.get("/", function (req, res) {
   const salida = {
     nombre: "Eddy",
     edad: 23,
@@ -12,6 +14,6 @@ app.get("/", function (req, res) {
 
 app.get("/user", function (req, res) {
     res.send('Hola User')
-  });
+  }); */
 
 app.listen(3000, ()=> {console.log(`Escuchando puerto 3000`);});
