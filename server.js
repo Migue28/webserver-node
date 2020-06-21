e7e776ba8a3c0d76263398380e4a93e0efc0883e
Eddy <miguepoke28@gmail.com>
2020-06-21 17:00:29 -0500

    HBS added, parcials made

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
-/* app.get("/", function (req, res) {
-  const salida = {
-    nombre: "Eddy",
-    edad: 23,
-    url: req.url,
-  };
-  res.send(salida)
+// Express HBS engine
+hbs.registerPartials(__dirname + "/views/parciales");
+app.set("view engine", "hbs");
+
+app.get("/", function (req, res) {
+  res.render("home.hbs", {
+    name: "Eddy",
+    year: new Date().getFullYear(),
+  });
 });
 
 app.get("/user", function (req, res) {
-    res.send('Hola User')
-  }); */
+  res.send("Hola User");
+});
 
-app.listen(3000, ()=> {console.log(`Escuchando puerto 3000`);});
+app.listen(3000, () => {
+  console.log(`Escuchando puerto 3000`);
+});
