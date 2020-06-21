8b00345e8d80e29f4e7bc25b3838b684ef3ec545
Eddy <miguepoke28@gmail.com>
2020-06-21 18:09:13 -0500

    Heroku added

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const hbs = require("hbs");
-require('./hbs/helpers')
+require("./hbs/helpers");
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
@@ -12,16 +14,13 @@ app.set("view engine", "hbs");
 app.get("/", function (req, res) {
   res.render("home.hbs", {
     name: "Eddy",
-    year: new Date().getFullYear(),
   });
 });
 
 app.get("/about", function (req, res) {
-  res.render("about.hbs", {
-    year: new Date().getFullYear(),
-  });
+  res.render("about.hbs");
 });
 
-app.listen(3000, () => {
-  console.log(`Escuchando puerto 3000`);
+app.listen(port, () => {
+  console.log(`Escuchando puerto ${port}`);
 });
